82498a9e46a641f741c10f59ef4eec1726023802
WildWill07 <100398202+WildWill07@users.noreply.github.com>
2024-05-03 13:45:42 -0500

    Final terminal flags disabled to enable raw mode.

--- ark.c
+++ ark.c
@@ -17,7 +17,10 @@ void enableRawMode() {
     atexit(disableRawMode); 
 
     struct termios raw = orig_termios;
-    raw.c_lflag &= ~(ECHO | ICANON);
+    raw.c_lflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON); // Disables Ctrl-S & Ctrl-Q Signals and Carriage returns (Ctrl-M)
+    raw.c_lflag &= ~(OPOST);
+    raw.c_lflag |= (CS8);
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG); //Disable Ctrl-C & Ctrl-Z Signals and Canonical Mode
 
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
@@ -26,12 +29,15 @@ int main() {
     enableRawMode();
 
     char c;
+    // Reads input signals and outpus their ASCII value
     while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+        // Control Characters 0-31 & 127
         if(iscntrl(c)) {
-            printf("%d\n", c);
+            printf("%d\r\n", c);
         }
+        // All other characters 32-126
         else {
-            printf("%d ('%c')\n", c, c);
+            printf("%d ('%c')\r\n", c, c);
         }
     }
     return 0;
