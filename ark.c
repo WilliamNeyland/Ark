e09e6a445ea30c61a8d03f29ecd072c14e80e84c
WildWill07 <100398202+WildWill07@users.noreply.github.com>
2024-05-03 14:01:53 -0500

    Completes Raw Mode.

--- ark.c
+++ ark.c
@@ -1,19 +1,33 @@
-#include <unistd.h>
-#include <termios.h>
-#include <stdlib.h>
+/*** includes ***/
+
 #include <ctype.h>
+#include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+
+/*** data ***/
 
 struct termios orig_termios;
 
+/*** terminal ***/
+
+void die(const char *s) {
+    perror(s);
+    exit(1);
+}
+
 // Restores terminal canonical mode
 void disableRawMode() {
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) {
+        die("tcsetattr");
+    }
 }
 
 // Disables terminal canonical mode and enables terminal raw mode
 void enableRawMode() {
-    tcgetattr(STDIN_FILENO, &orig_termios);
+    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
     atexit(disableRawMode); 
 
     struct termios raw = orig_termios;
@@ -21,24 +35,30 @@ void enableRawMode() {
     raw.c_lflag &= ~(OPOST);
     raw.c_lflag |= (CS8);
     raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG); //Disable Ctrl-C & Ctrl-Z Signals and Canonical Mode
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
 
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
+
+/*** init ***/
 
 int main() {
     enableRawMode();
 
-    char c;
     // Reads input signals and outpus their ASCII value
-    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+    while (1) {
+        char c = '\0';
+        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
         // Control Characters 0-31 & 127
-        if(iscntrl(c)) {
+        if (iscntrl(c)) {
             printf("%d\r\n", c);
         }
         // All other characters 32-126
         else {
             printf("%d ('%c')\r\n", c, c);
         }
+        if (c == 'q') break;
     }
     return 0;
 }
