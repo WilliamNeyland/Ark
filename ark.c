e7124cde65917c8f21a5e9f1494fffde661212d2
WildWill07 <100398202+WildWill07@users.noreply.github.com>
2024-05-07 21:04:10 -0500

    Adds editorConfig global struct and functionality to return terminal size

--- ark.c
+++ ark.c
@@ -4,6 +4,7 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
 
@@ -12,8 +13,13 @@
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 /*** data ***/
+struct editorConfig {
+    int screenrows;
+    int screencols;
+    struct termios orig_termios;
+};
 
-struct termios orig_termios;
+struct editorConfig E;
 
 /*** terminal ***/
 
@@ -27,16 +33,16 @@ void die(const char *s) {
 
 // Restores terminal canonical mode
 void disableRawMode() {
-    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1)
         die("tcsetattr");
 }
 
 // Disables terminal canonical mode and enables terminal raw mode
 void enableRawMode() {
-    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
+    if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1) die("tcgetattr");
     atexit(disableRawMode); 
 
-    struct termios raw = orig_termios;
+    struct termios raw = E.orig_termios;
     raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON); // Disables Ctrl-S & Ctrl-Q Signals and Carriage returns (Ctrl-M)
     raw.c_oflag &= ~(OPOST);
     raw.c_cflag |= (CS8);
@@ -54,6 +60,20 @@ char editorReadKey() {
         if (nread == -1 && errno != EAGAIN) die("read");
     }
     return c;
+}
+
+// Utilizes ioctl.h library to return terminal size in columns by rows
+int getWindowSize(int *rows, int *cols) {
+    struct winsize ws;
+
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col ==0) {
+        return -1;
+    }
+    else {
+        *cols = ws.ws_col;
+        *rows = ws.ws_row;
+        return 0;
+    }
 }
 
 /*** output ***/
@@ -91,8 +111,13 @@ void editorProcessKeypress() {
 
 /*** init ***/
 
+void initEditor() {
+    if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
+}
+
 int main() {
     enableRawMode();
+    initEditor();
 
     while(1) {
         editorRefreshScreen();
